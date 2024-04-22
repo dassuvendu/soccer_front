326791afa48d69fdde537951b87d9d70c2c07268
Suvendu <suvendu.iksen@gmail.com>
2024-04-22 19:41:33 +0530

    msg

--- src/pages/CookedSlips/SlipInfo.jsx
+++ src/pages/CookedSlips/SlipInfo.jsx
@@ -1,15 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
-import { getSlipInfo } from "../../reducers/CookedSlipSlice";
+import { getSlipInfo, getslipDetails } from "../../reducers/CookedSlipSlice";
 import { Table } from "flowbite-react";
 import { MdMoreHoriz } from "react-icons/md";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { logoIcon } from "../../assets/images/images";
+import { PredictionRequestModal } from "../MyPrediction/PredictionCompo/PredictionRequestModal";
 
 const SlipInfo = () => {
+  const [fixturesId, setFixturesId] = useState();
+  const [homeId, setHomeId] = useState();
+  const [awayId, setAwayId] = useState();
+  const [timeStamp, setTimeStamp] = useState(null);
+  const [openDetailsModal, setOpenDetailsModal] = useState(false);
   const themeMode = useSelector((state) => state.darkmode.mode);
-  const { slipInfo, isLoading } = useSelector((state) => state.cookedSlips);
+  const { isLoading, singleSlip } = useSelector((state) => state.cookedSlips);
   const location = useLocation();
   let id;
   if (location?.state?.id) {
@@ -18,9 +24,9 @@ const SlipInfo = () => {
   console.log("location", location);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getSlipInfo({ id: id }));
-  }, []);
-  console.log("slip Info: ", slipInfo?.data);
+    dispatch(getslipDetails({ id: id }));
+  }, [dispatch]);
+  console.log("slip Info:", singleSlip?.data);
 
   const matchStartDate = (dateString) => {
     const options = {
@@ -82,7 +88,20 @@ const SlipInfo = () => {
     };
     return date.toLocaleTimeString(undefined, options);
   };
-
+  const viewDetailsModalHandler = (id, hid, aid, timeId) => {
+    setFixturesId(id);
+    setHomeId(hid);
+    setAwayId(aid);
+    setTimeStamp(timeId);
+    setOpenDetailsModal(true);
+    console.log("fixture id: ", id);
+    console.log("home id: ", hid);
+    console.log("away id: ", aid);
+    console.log("time id: ", timeId);
+  };
+  const handleModalClose = () => {
+    setOpenDetailsModal(false);
+  };
   return (
     <>
       <div className="flex justify-between items-center mb-6">
@@ -105,7 +124,7 @@ const SlipInfo = () => {
                 themeMode === "light" ? "text-[#191D23]" : "text-white"
               }`}
             >
-              {slipInfo?.data?.count} Matches
+              {singleSlip?.data?.count} Matches
             </h2>
           </div>
           <div>
@@ -117,7 +136,7 @@ const SlipInfo = () => {
                 themeMode === "light" ? "text-[#191D23]" : "text-white"
               }`}
             >
-              {slipInfo?.data?.odds}
+              {singleSlip?.data?.odds}
             </h2>
           </div>
           <div>
@@ -128,17 +147,17 @@ const SlipInfo = () => {
             >
               Risk Level
             </p>
-            {slipInfo?.data?.risk === "medium" ? (
+            {singleSlip?.data?.risk === "medium" ? (
               <span className="text-[16px] leading-[30px] font-medium text-black bg-[#fba930] py-0 px-3 inline-block rounded-full">
-                {slipInfo?.data?.risk} risk
+                {singleSlip?.data?.risk} risk
               </span>
-            ) : slipInfo?.data?.risk === "low" ? (
+            ) : singleSlip?.data?.risk === "low" ? (
               <span className="text-[16px] leading-[30px] font-medium text-black bg-[#22E810] py-0 px-3 inline-block rounded-full">
-                {slipInfo?.data?.risk} risk
+                {singleSlip?.data?.risk} risk
               </span>
             ) : (
               <span className="text-[16px] leading-[30px] font-medium text-white bg-[#ff0000] py-0 px-3 inline-block rounded-full">
-                {slipInfo?.data?.risk} risk
+                {singleSlip?.data?.risk} risk
               </span>
             )}
           </div>
@@ -151,10 +170,10 @@ const SlipInfo = () => {
                 themeMode === "light" ? "text-[#191D23]" : "text-white"
               }`}
             >
-              {slipInfo?.data?.strategy}
+              {singleSlip?.data?.strategy}
             </h3>
           </div>
-          <div>
+          {/* <div>
             <p className="font-Montserrat text-[14px] leading-[18px] font-medium text-[#2aa9e1] mb-1">
               Max Stake
             </p>
@@ -165,7 +184,7 @@ const SlipInfo = () => {
             >
               {slipInfo?.data?.maxStake}
             </h3>
-          </div>
+          </div> */}
           <div>
             <p className="font-Montserrat text-[14px] leading-[18px] font-medium text-[#2aa9e1] mb-1">
               Starts On
@@ -175,8 +194,8 @@ const SlipInfo = () => {
                 themeMode === "light" ? "text-[#191D23]" : "text-white"
               }`}
             >
-              {formatStartDate(slipInfo?.data?.startsOn)}{" "}
-              {formatStartTime(slipInfo?.data?.startsOn)}
+              {formatStartDate(singleSlip?.data?.startsOn)}{" "}
+              {formatStartTime(singleSlip?.data?.startsOn)}
             </h3>
           </div>
           <div>
@@ -188,8 +207,8 @@ const SlipInfo = () => {
                 themeMode === "light" ? "text-[#191D23]" : "text-white"
               }`}
             >
-              {formatEndDate(slipInfo?.data?.endsOn)}{" "}
-              {formatEndTime(slipInfo?.data?.endsOn)}
+              {formatEndDate(singleSlip?.data?.endsOn)}{" "}
+              {formatEndTime(singleSlip?.data?.endsOn)}
             </h3>
           </div>
         </div>
@@ -215,7 +234,7 @@ const SlipInfo = () => {
                 >
                   Match
                 </Table.HeadCell>
-                <Table.HeadCell
+                {/* <Table.HeadCell
                   className={`${
                     themeMode === "light" ? "bg-white" : "bg-[#191D23]"
                   } text-[16px] ${
@@ -223,15 +242,15 @@ const SlipInfo = () => {
                   } font-medium capitalize w-[17%]`}
                 >
                   Advice
-                </Table.HeadCell>
+                </Table.HeadCell> */}
 
-                <Table.HeadCell
+                {/* <Table.HeadCell
                   className={`${
                     themeMode === "light" ? "bg-white" : "bg-[#191D23]"
                   } text-[16px] ${
                     themeMode === "light" ? "text-[#787a7d]" : "text-[#96A5B8]"
                   } font-medium capitalize w-[17%]`}
-                ></Table.HeadCell>
+                ></Table.HeadCell> */}
                 <Table.HeadCell
                   className={`${
                     themeMode === "light" ? "bg-white" : "bg-[#191D23]"
@@ -253,22 +272,6 @@ const SlipInfo = () => {
                             alt="loading.."
                             className="loader"
                           />
-                          {/* <svg
-              aria-hidden="true"
-              class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg> */}
                           <span className="sr-only">Loading...</span>
                         </div>
                       </div>
@@ -276,7 +279,7 @@ const SlipInfo = () => {
                   </tr>
                 ) : (
                   <>
-                    {slipInfo?.data?.items?.map((predict) => {
+                    {singleSlip?.data?.items?.map((predict) => {
                       return (
                         <>
                           <Table.Row
@@ -313,12 +316,12 @@ const SlipInfo = () => {
                                 </div>
                               </div>
                             </Table.Cell>
-                            <Table.Cell className="w-[17%]">
+                            {/* <Table.Cell className="w-[17%]">
                               <span className=" text-black font-medium text-[15px] leading-[30px] font-Montserrat inline-block px-6">
                                 {predict?.advice}
                               </span>
-                            </Table.Cell>
-                            <Table.Cell className="w-[5%]">
+                            </Table.Cell> */}
+                            {/* <Table.Cell className="w-[5%]">
                               <span
                                 className={`text-base font-bold ${
                                   themeMode === "light"
@@ -376,18 +379,18 @@ const SlipInfo = () => {
                                   )}{" "}
                                 </span>
                               </span>
-                            </Table.Cell>
+                            </Table.Cell> */}
 
                             <Table.Cell className="text-center text-2xl cursor-pointer w-[15%]">
                               <MdMoreHoriz
-                              // onClick={() =>
-                              //   viewDetailsModalHandler(
-                              //     predict?.fixture_id,
-                              //     predict?.teams?.home?.id,
-                              //     predict?.teams?.away?.id,
-                              //     predict?.fixture_date
-                              //   )
-                              // }
+                                onClick={() =>
+                                  viewDetailsModalHandler(
+                                    predict?.fixtureId,
+                                    predict?.teams?.home?.id,
+                                    predict?.teams?.away?.id,
+                                    predict?.fixture_date
+                                  )
+                                }
                               />
                             </Table.Cell>
                           </Table.Row>
@@ -400,6 +403,14 @@ const SlipInfo = () => {
             </Table>
           </div>
           {/* Table ends here */}
+          <PredictionRequestModal
+            openDetailsModal={openDetailsModal}
+            onClose={handleModalClose}
+            fixturesId={fixturesId}
+            timeStamp={timeStamp}
+            homeId={homeId}
+            awayId={awayId}
+          />
         </TabPanel>
       </Tabs>
     </>
