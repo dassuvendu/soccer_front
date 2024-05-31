7922b68a7268f25057020c340b171a6ac98a21a3
sourav16716 <54978571+sourav16716@users.noreply.github.com>
2024-05-31 14:53:50 +0530

    Update plan page

--- src/pages/ChoosePlan/plan.jsx
+++ src/pages/ChoosePlan/plan.jsx
@@ -227,12 +227,10 @@ const Plan = () => {
     setShowSubscription(false);
   };
 
-
   const createMonnifySubscription = () => {
     setShowMonnifyPayment(true);
     setShowSubscription(false);
-  }
-
+  };
 
   useEffect(() => {
     dispatch(subscriptionPlans()).then(() => {
@@ -260,36 +258,34 @@ const Plan = () => {
               <div className="max-w-7xl mx-auto py-0 lg:py-4 px-0">
                 <div className="plan_tab_area">
                   <div className="px-4 lg:px-0">
-                    <div className="w-full max-w-4xl p-6 mx-auto my-0 shadow-xl bg-[#2aa9e1] rounded-2xl lg:p-10">
+                    <div className="w-full max-w-4xl p-6 mx-auto my-0 lg:p-10">
                       <div className="container mx-auto my-0">
-                        <div className="md:flex">
-                          <div className="w-2/5 hidden lg:block">
-                            <div className="text-center">
-                              <img
-                                src={planIcon}
-                                alt="planIcon"
-                                className="rounded-xl"
-                              />
-                            </div>
-                          </div>
-                          <div className="w-full lg:w-3/5">
+                        <div>
+                          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                             {plans &&
                               plans.length > 0 &&
                               plans?.map((plan, plankey) => (
                                 <div
                                   key={"plan_" + plankey}
-                                  className="lg:px-28"
+                                  className="px-10 py-12  shadow-xl bg-[#2aa9e1] rounded-2xl"
                                 >
-                                  <h2 className="font-Bebas text-white py-5 text-6xl lg:text-8xl tracking-normal mb-0 text-center">
+                                  <div className="text-center">
+                                    <img
+                                      src={planIcon}
+                                      alt="planIcon"
+                                      className="rounded-xl w-48 inline-block"
+                                    />
+                                  </div>
+                                  <h2 className="font-Bebas text-white py-5 text-2xl lg:text-4xl tracking-normal mb-0 text-center">
                                     Choose Plan
                                   </h2>
                                   <div className="text-center">
-                                    <h3 className="text-2xl text-white font-semibold mb-4">
+                                    <h3 className="text-xl lg:text-2xl text-white font-semibold mb-4">
                                       {/* 10$ monthly only */}
                                       {plan?.price}$ monthly only
                                     </h3>
                                     <button
-                                      className="text-base font-medium hover:bg-[#18191b] text-white text-center w-full block border-2 py-2 border-white hover:border-[#18191b]"
+                                      className="text-base font-medium hover:bg-[#18191b] text-white text-center rounded-lg w-full block border-2 py-2 border-white hover:border-[#18191b]"
                                       // onClick={() => {
                                       //   createSubscription(plan.id, userId);
                                       //   localStorage.setItem("planId", plan.id);
@@ -334,14 +330,13 @@ const Plan = () => {
           />
         )}
 
-      {showMonnifyPayment &&
-        (
-          <MonnifyPayment
-            planId={planId}
-            email={userDetails.email}
-            user_id={user_id}
-          />
-        )}
+      {showMonnifyPayment && (
+        <MonnifyPayment
+          planId={planId}
+          email={userDetails.email}
+          user_id={user_id}
+        />
+      )}
 
       {openLoginModal && (
         <Login
@@ -369,7 +364,9 @@ const Plan = () => {
               </div>
               <div className="w-full md:w-6/12 md:pl-4">
                 <div className="text-center">
-                  <h2 className="mb-8 text-xl font-bold text-[#2aa9e1]">Choose one payment method </h2>
+                  <h2 className="mb-8 text-xl font-bold text-[#2aa9e1]">
+                    Choose one payment method{" "}
+                  </h2>
                   <button
                     className="flex justify-center items-center rounded-xl text-base font-medium hover:bg-[#2aa9e1] text-[#111111] text-center w-full border-2 py-2 border-[#2aa9e1] hover:border-[#2aa9e1]"
                     onClick={() => {
@@ -386,7 +383,6 @@ const Plan = () => {
                       createMonnifySubscription(planId, userId);
                       setOpenChoosePaymentModal(false);
                     }}
-
                   >
                     <IoRadioButtonOnSharp className="mr-1" /> Monnify
                   </button>
@@ -396,8 +392,6 @@ const Plan = () => {
           </Modal.Body>
         </Modal>
       )}
-
-
     </div>
   );
 };
