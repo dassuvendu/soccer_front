303827e28debf5fad1809fc50d9b91a49cba6252
Suvendu <suvendu.iksen@gmail.com>
2024-06-04 13:08:14 +0530

    msg

--- src/pages/Home/HomeBannerMatchStartTime.jsx
+++ src/pages/Home/HomeBannerMatchStartTime.jsx
@@ -4,27 +4,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { getFixtures } from "../../reducers/PredictionsSlice";
 
 const HomeBannerMatchStartTime = () => {
-
   const { fixtures } = useSelector((state) => state.prediction);
 
-  const [time, setTime] = useState('');
+  const [time, setTime] = useState("");
   // console.log("time",time);
   // const formateTime = time.toString().split("+")[0]
   // console.log("next time",formateTime);
-  const [countdown, setCountdown] = useState({ days: '00', hours: '00', minutes: '00', seconds: '00', period: 'AM' });
+  const [countdown, setCountdown] = useState({
+    days: "00",
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+    period: "AM",
+  });
   // console.log("coun",countdown);
-  const [diff ,setDiff] = useState()
+  const [diff, setDiff] = useState();
   // console.log("diff",diff);
   const [matchStarted, setMatchStarted] = useState(false);
-  const [nxtMatch, setNextMatch] = useState()
-  // console.log("nxt",nxtMatch);
-  // useEffect(() => {
-  //   if (items && items.length > 0) {
-  //     const date = new Date(items[0]?.fixture?.date);
-  //     setTime(date.toISOString().split(".")[0]); // Convert date to ISO string
-  //   }
-  // }, [items]);
-  
+  const [nxtMatch, setNextMatch] = useState();
+
   const today = new Date();
   const todayFormatted = today.toISOString().split("T")[0];
 
@@ -32,57 +30,68 @@ const HomeBannerMatchStartTime = () => {
 
   useEffect(() => {
     // Check if any matches are ongoing
-    const ongoingMatch = fixtures?.data?.filter(item => {
-    
+    if (!fixtures || !fixtures.data) return;
+    const ongoingMatch = fixtures?.data?.filter((item) => {
       const fixtureDate = new Date(item?.fixture?.date);
       const currentUTCTime = new Date().toISOString();
-      
-      fixtureDate.setMinutes(fixtureDate.getMinutes() + fixtureDate.getTimezoneOffset());
-      const currentUTCTimeWithoutMs = currentUTCTime.split('.')[0] + 'Z';
-  
+
+      fixtureDate.setMinutes(
+        fixtureDate.getMinutes() + fixtureDate.getTimezoneOffset()
+      );
+      const currentUTCTimeWithoutMs = currentUTCTime.split(".")[0] + "Z";
+
       return fixtureDate > new Date(currentUTCTimeWithoutMs);
     });
-    
-    if (ongoingMatch) {
-        
-        // Filter out the next match
-        const Item = Array.isArray(fixtures?.data) && fixtures?.data?.filter(item => {
-    
-            const fixtureDate = new Date(item?.fixture?.date);
-            const currentUTCTime = new Date().toISOString();
-            
-            fixtureDate.setMinutes(fixtureDate.getMinutes() + fixtureDate.getTimezoneOffset());
-            const currentUTCTimeWithoutMs = currentUTCTime.split('.')[0] + 'Z';
-        
-            return fixtureDate > new Date(currentUTCTimeWithoutMs);
-        });
-       
-        setNextMatch(Item[0]);
-        
-        if (Item) {
-            const londonTime = new Date(Item[0].fixture.date).toLocaleString("en-US",{timeZone: 'Europe/London'});
-            setTime(londonTime);
-        }
- 
-    } else {
-        // If no match is ongoing, find the next match
-        const Item =  Array.isArray(fixtures?.data) && fixtures?.data?.filter(item => {
-    
-            const fixtureDate = new Date(item?.fixture?.date);
-            const currentUTCTime = new Date().toISOString();
-            
-            fixtureDate.setMinutes(fixtureDate.getMinutes() + fixtureDate.getTimezoneOffset());
-            const currentUTCTimeWithoutMs = currentUTCTime.split('.')[0] + 'Z';
-        
-            return fixtureDate > new Date(currentUTCTimeWithoutMs);
-        });
-        if (Item) {
-            const londonTime = new Date(Item[0].fixture.date).toLocaleString("en-US",{timeZone: 'Europe/London'});
-            setTime(londonTime);
-        }
-    }
-}, [fixtures]);
 
+    if (ongoingMatch) {
+      // Filter out the next match
+      const Item =
+        Array.isArray(fixtures?.data) &&
+        fixtures?.data?.filter((item) => {
+          const fixtureDate = new Date(item?.fixture?.date);
+          const currentUTCTime = new Date().toISOString();
+
+          fixtureDate.setMinutes(
+            fixtureDate.getMinutes() + fixtureDate.getTimezoneOffset()
+          );
+          const currentUTCTimeWithoutMs = currentUTCTime.split(".")[0] + "Z";
+
+          return fixtureDate > new Date(currentUTCTimeWithoutMs);
+        });
+
+      setNextMatch(Item[0]);
+
+      if (Item) {
+        const londonTime = new Date(Item[0].fixture.date).toLocaleString(
+          "en-US",
+          { timeZone: "Europe/London" }
+        );
+        setTime(londonTime);
+      }
+    } else {
+      // If no match is ongoing, find the next match
+      const Item =
+        Array.isArray(fixtures?.data) &&
+        fixtures?.data?.filter((item) => {
+          const fixtureDate = new Date(item?.fixture?.date);
+          const currentUTCTime = new Date().toISOString();
+
+          fixtureDate.setMinutes(
+            fixtureDate.getMinutes() + fixtureDate.getTimezoneOffset()
+          );
+          const currentUTCTimeWithoutMs = currentUTCTime.split(".")[0] + "Z";
+
+          return fixtureDate > new Date(currentUTCTimeWithoutMs);
+        });
+      if (Item) {
+        const londonTime = new Date(Item[0].fixture.date).toLocaleString(
+          "en-US",
+          { timeZone: "Europe/London" }
+        );
+        setTime(londonTime);
+      }
+    }
+  }, [fixtures]);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -97,35 +106,54 @@ const HomeBannerMatchStartTime = () => {
     // console.log('time->', time)
     if (!time) {
       // console.log("!time");
-      return { days: '00', hours: '00', minutes: '00', seconds: '00', period: 'AM' };
+      return {
+        days: "00",
+        hours: "00",
+        minutes: "00",
+        seconds: "00",
+        period: "AM",
+      };
     }
-    
+
     const targetDate = new Date(time);
     // console.log("tar",targetDate);
     const now = new Date();
     // console.log("tar1",now);
     const difference = targetDate - now;
-    setDiff(difference)
+    setDiff(difference);
     // console.log("d",difference);
     if (difference < 0) {
-      
-      return { days: '00', hours: '00', minutes: '00', seconds: '00', period: 'AM' };
+      return {
+        days: "00",
+        hours: "00",
+        minutes: "00",
+        seconds: "00",
+        period: "AM",
+      };
     }
 
- // Convert milliseconds to hours, minutes, and seconds
-const totalSeconds = Math.floor(difference / 1000);
-const days = Math.floor(totalSeconds / (3600 * 24)).toString().padStart(2, '0');
-// console.log("hrsD",days);
-let hours = Math.floor((totalSeconds % (3600 * 24)) / 3600).toString().padStart(2, '0');
-// console.log("hrsH",hours);
-const minutes = Math.floor((totalSeconds % 3600) / 60).toString().padStart(2, '0');
-// console.log("hrsM",minutes);
-const seconds = Math.floor(totalSeconds % 60).toString().padStart(2, '0');
+    // Convert milliseconds to hours, minutes, and seconds
+    const totalSeconds = Math.floor(difference / 1000);
+    const days = Math.floor(totalSeconds / (3600 * 24))
+      .toString()
+      .padStart(2, "0");
+    // console.log("hrsD",days);
+    let hours = Math.floor((totalSeconds % (3600 * 24)) / 3600)
+      .toString()
+      .padStart(2, "0");
+    // console.log("hrsH",hours);
+    const minutes = Math.floor((totalSeconds % 3600) / 60)
+      .toString()
+      .padStart(2, "0");
+    // console.log("hrsM",minutes);
+    const seconds = Math.floor(totalSeconds % 60)
+      .toString()
+      .padStart(2, "0");
 
-return { days, hours, minutes, seconds};
+    return { days, hours, minutes, seconds };
   }
   // const [apiCalled, setApiCalled] = useState(false);
-  const [color,  setColor] = useState(false);
+  const [color, setColor] = useState(false);
   // useEffect(() => {
   //   let timeoutId;
 
@@ -140,7 +168,7 @@ return { days, hours, minutes, seconds};
 
   useEffect(() => {
     let intervalId;
-  
+
     // Function to fetch fixtures
     const fetchFixtures = () => {
       dispatch(getFixtures({ date: todayFormatted })).then((res) => {
@@ -149,14 +177,14 @@ return { days, hours, minutes, seconds};
         }
       });
     };
-  
+
     // Function to reset countdown and fetch fixtures
     const resetCountdown = () => {
       setDiff(30000); // Set diff to 30 seconds
       fetchFixtures(); // Fetch fixtures immediately
       intervalId = setInterval(fetchFixtures, 30000); // Start interval
     };
-  
+
     // Start interval if diff < 0
     if (diff <= 0) {
       setMatchStarted(true);
@@ -167,10 +195,10 @@ return { days, hours, minutes, seconds};
         setDiff((prevDiff) => prevDiff - 1000); // Decrease diff every second
       }, 1000);
     }
-  
+
     // Set color based on diff
     setColor(diff <= 10000);
-  
+
     // Clean up function to clear interval
     return () => {
       clearInterval(intervalId);
@@ -178,101 +206,109 @@ return { days, hours, minutes, seconds};
   }, [diff, todayFormatted, dispatch, setDiff, setMatchStarted]);
 
   useEffect(() => {
-    dispatch(getFixtures({ date: todayFormatted }))
+    dispatch(getFixtures({ date: todayFormatted }));
   }, [todayFormatted]);
-  
+
   return (
     <div className="mt-20 mb-[20px] relative z-10 md:px-4 lg:px-0">
       <div className="max-full mx-auto">
         <div className="md:flex justify-center items-center">
           <div className="flex items-center justify-center mb-4 md:mb-0">
-            <img src={nxtMatch?.teams?.home?.logo} width={100} alt={ nxtMatch?.teams?.home?.name}/>
+            <img
+              src={nxtMatch?.teams?.home?.logo}
+              width={100}
+              alt={nxtMatch?.teams?.home?.name}
+            />
             <div className="ml-2 text-right">
               <h2 className="font-Bebas text-[#18191b] text-3xl tracking-normal">
-              { nxtMatch?.teams?.home?.name}
+                {nxtMatch?.teams?.home?.name}
               </h2>
               <p className="text-[#9c9da1] text-[12px] italic">
-              { nxtMatch?.league?.name}
+                {nxtMatch?.league?.name}
               </p>
             </div>
           </div>
           {matchStarted ? (
-        <div>
-          <h1 className="p-5 text-lg text-[red] text-[15px] leading-[15px] font-medium m-2">Match is live!</h1>
-        
-        </div>
-      ):  (
-          <div className="flex justify-center items-center mx-16 mb-4 md:mb-0">
-            <ul className="flex justify-center items-center">
-              <li className="text-center mx-2">
-                <div className="flex justify-center items-center">
-                  <span className="bg-[#282828] mr-0.5 font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
-                  {countdown?.days}
-                  </span>
-                  {/* <span className="bg-[#282828] font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
+            <div>
+              <h1 className="p-5 text-lg text-[red] text-[15px] leading-[15px] font-medium m-2">
+                Match is live!
+              </h1>
+            </div>
+          ) : (
+            <div className="flex justify-center items-center mx-16 mb-4 md:mb-0">
+              <ul className="flex justify-center items-center">
+                <li className="text-center mx-2">
+                  <div className="flex justify-center items-center">
+                    <span className="bg-[#282828] mr-0.5 font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
+                      {countdown?.days}
+                    </span>
+                    {/* <span className="bg-[#282828] font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
                 {countdown?.days?.toString().split("-")[1]}
                   </span> */}
-                </div>
-                <p className="text-[#282828] text-[10px] pt-0.5">Days</p>
-              </li>
-              :
-              <li className="text-center mx-2">
-                <div className="flex justify-center items-center">
-                  <span className="bg-[#282828] mr-0.5 font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
-                  {countdown?.hours}
-                  </span>
-                  {/* <span className="bg-[#282828] font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
+                  </div>
+                  <p className="text-[#282828] text-[10px] pt-0.5">Days</p>
+                </li>
+                :
+                <li className="text-center mx-2">
+                  <div className="flex justify-center items-center">
+                    <span className="bg-[#282828] mr-0.5 font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
+                      {countdown?.hours}
+                    </span>
+                    {/* <span className="bg-[#282828] font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
                 2
                   </span> */}
-                </div>
-                <p className="text-[#282828] text-[10px] pt-0.5">Hours</p>
-              </li>
-              :
-              <li className="text-center mx-2">
-                <div className="flex justify-center items-center">
-                  <span className="bg-[#282828] mr-0.5 font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
-                  {countdown.minutes}
-                  </span>
-                  {/* <span className="bg-[#282828] font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
+                  </div>
+                  <p className="text-[#282828] text-[10px] pt-0.5">Hours</p>
+                </li>
+                :
+                <li className="text-center mx-2">
+                  <div className="flex justify-center items-center">
+                    <span className="bg-[#282828] mr-0.5 font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
+                      {countdown.minutes}
+                    </span>
+                    {/* <span className="bg-[#282828] font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
                   2
                   </span> */}
-                </div>
-                <p className="text-[#282828] text-[10px] pt-0.5">Minutes</p>
-              </li>
-              :
-              <li className="text-center mx-2">
-                <div className="flex justify-center items-center">
-                  {color ? 
-                  <span className=" flex  bg-[#282828] rounded-t-sm">
-                  <span className=" animate-pulse mr-0.6 font-Bebas font-medium tracking-normal text-[yellow] text-[15px] leading-[15px] px-2 pt-2.5 pb-0">
-                  {countdown.seconds}
-                  </span>
-                </span>
-                  
-                  :
-                  <span className="bg-[#282828] mr-0.5 font-Bebas font-medium tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] px-2 pt-2.5 pb-0">
-                  {countdown.seconds}
-                  </span>
-                  }
-                  {/* <span className="bg-[#282828] font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
+                  </div>
+                  <p className="text-[#282828] text-[10px] pt-0.5">Minutes</p>
+                </li>
+                :
+                <li className="text-center mx-2">
+                  <div className="flex justify-center items-center">
+                    {color ? (
+                      <span className=" flex  bg-[#282828] rounded-t-sm">
+                        <span className=" animate-pulse mr-0.6 font-Bebas font-medium tracking-normal text-[yellow] text-[15px] leading-[15px] px-2 pt-2.5 pb-0">
+                          {countdown.seconds}
+                        </span>
+                      </span>
+                    ) : (
+                      <span className="bg-[#282828] mr-0.5 font-Bebas font-medium tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] px-2 pt-2.5 pb-0">
+                        {countdown.seconds}
+                      </span>
+                    )}
+                    {/* <span className="bg-[#282828] font-Bebas tracking-normal rounded-t-sm text-white text-[15px] leading-[15px] font-medium px-2 pt-2.5 pb-0">
                   7
                   </span> */}
-                </div>
-                <p className="text-[#282828] text-[10px] pt-0.5">Seconds</p>
-              </li>
-            </ul>
-          </div>
-      )}
+                  </div>
+                  <p className="text-[#282828] text-[10px] pt-0.5">Seconds</p>
+                </li>
+              </ul>
+            </div>
+          )}
           <div className="flex items-center justify-center">
             <div className="mr-2 text-left">
               <h2 className="font-Bebas text-[#18191b] text-3xl tracking-normal">
-              {nxtMatch?.teams?.away?.name}
+                {nxtMatch?.teams?.away?.name}
               </h2>
               <p className="text-[#9c9da1] text-[12px] italic">
-              {nxtMatch?.league?.name}
+                {nxtMatch?.league?.name}
               </p>
             </div>
-            <img src={nxtMatch?.teams?.away?.logo} width={100} alt={nxtMatch?.teams?.away?.name} />
+            <img
+              src={nxtMatch?.teams?.away?.logo}
+              width={100}
+              alt={nxtMatch?.teams?.away?.name}
+            />
           </div>
         </div>
       </div>
--- src/pages/Home/PastMatch.jsx
+++ src/pages/Home/PastMatch.jsx
@@ -11,7 +11,9 @@ export const PastMatch = () => {
   const { pastFix, isLoading } = useSelector((state) => state.prediction);
 
   const Items = pastFix?.data?.slice(0, 6);
+  console.log("Item", Items);
   // console.log("y", Items);
+  const { lastResult } = useSelector((state) => state.prediction);
 
   const today = new Date();
   // const todayFormatted = today.toISOString().split("T")[0];
@@ -81,95 +83,90 @@ export const PastMatch = () => {
     setTimeStamp(null);
     setFixturesId(null);
   };
+
   return (
     <div>
       <div className="pt-4">
         {!isLoading ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {Array.isArray(Items) &&
-              Items?.map((data) => (
-                <div className="bg-white rounded-2xl shadow-xl" key={data.id}>
-                  <div className="flex justify-between items-center bg-gray-800 px-5 py-3 rounded-t-2xl h-16">
-                    <div className="text-white font-bold text-[16px] leading-[20px] font-Montserrat">
-                      Match Details
-                    </div>
-                    <div className="text-right">
-                      <p className="text-white font-medium text-[12px] leading-[16px] font-Montserrat">
-                        Friendlies | {formatDay(data?.fixture?.timestamp)},
-                      </p>
-                      <p className="text-white font-medium text-[12px] leading-[16px] font-Montserrat">
-                        {formatDate(data?.fixture?.timestamp)}
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="pt-6 pb-4 px-3 border-2 border-gray-800 rounded-b-2xl">
-                    <div className="grid grid-cols-3 gap-4 mb-4 h-32">
-                      <div className="text-center">
-                        <img
-                          src={data?.teams?.home?.logo}
-                          alt={data?.teams?.home?.name}
-                          className="inline-block mb-2"
-                          width={58}
-                          height={58}
-                        />
-                        <p className="font-Syne text-[14px] leading-[20px] font-bold text-black">
-                          {data?.teams?.home?.name}
+              Items?.map((data) => {
+                return (
+                  <div className="bg-white rounded-2xl shadow-xl" key={data.id}>
+                    <div className="flex justify-between items-center bg-gray-800 px-5 py-3 rounded-t-2xl h-16">
+                      <div className="text-white font-bold text-[16px] leading-[20px] font-Montserrat">
+                        Match Details
+                      </div>
+                      <div className="text-right">
+                        <p className="text-white font-medium text-[12px] leading-[16px] font-Montserrat">
+                          Friendlies | {formatDay(data?.fixture?.timestamp)},
+                        </p>
+                        <p className="text-white font-medium text-[12px] leading-[16px] font-Montserrat">
+                          {formatDate(data?.fixture?.timestamp)}
                         </p>
                       </div>
-                      <div className="flex justify-center items-center">
-                        <div>
-                          <div className="mb-4 text-center">
-                            <p className="text-black font-semibold text-[12px] leading-[16px] font-Montserrat pb-1">
-                              Score
-                            </p>
-                            <span className="bg-gray-800 text-white font-medium text-[15px] leading-[25px] font-Montserrat inline-block px-4 py-0 rounded">
-                              <b>
-                                {data?.goals?.home} - {data?.goals?.away}
-                              </b>
-                            </span>
-                          </div>
-                          <div className="mb-4 text-center">
-                            {/* <p className="text-[#08a5f5] font-semibold text-[12px] leading-[16px] font-Montserrat pb-1">
-                              Prediction
-                            </p> */}
-                            {/* <span className="bg-[#08a5f5] text-white font-medium text-[15px] leading-[25px] font-Montserrat inline-block px-4 py-0 rounded">
-                              <b>2 - 0</b>
-                            </span> */}
+                    </div>
+
+                    <div className="pt-6 pb-4 px-3 border-2 border-gray-800 rounded-b-2xl">
+                      <div className="grid grid-cols-3 gap-4 mb-4 h-32">
+                        <div className="text-center">
+                          <img
+                            src={data?.teams?.home?.logo}
+                            alt={data?.teams?.home?.name}
+                            className="inline-block mb-2"
+                            width={58}
+                            height={58}
+                          />
+                          <p className="font-Syne text-[14px] leading-[20px] font-bold text-black">
+                            {data?.teams?.home?.name}
+                          </p>
+                        </div>
+                        <div className="flex justify-center items-center">
+                          <div>
+                            <div className="mb-4 text-center">
+                              <p className="text-black font-semibold text-[12px] leading-[16px] font-Montserrat pb-1">
+                                Score
+                              </p>
+                              <span className="bg-gray-800 text-white font-medium text-[15px] leading-[25px] font-Montserrat inline-block px-4 py-0 rounded">
+                                <b>
+                                  {data?.goals?.home} - {data?.goals?.away}
+                                </b>
+                              </span>
+                            </div>
                           </div>
                         </div>
+                        <div className="text-center">
+                          <img
+                            src={data?.teams?.away?.logo}
+                            alt={data?.teams?.away?.name}
+                            className="inline-block mb-2"
+                            width={58}
+                            height={58}
+                          />
+                          <p className="font-Syne text-[14px] leading-[20px] font-bold text-black">
+                            {data?.teams?.away?.name}
+                          </p>
+                        </div>
                       </div>
-                      <div className="text-center">
-                        <img
-                          src={data?.teams?.away?.logo}
-                          alt={data?.teams?.away?.name}
-                          className="inline-block mb-2"
-                          width={58}
-                          height={58}
-                        />
-                        <p className="font-Syne text-[14px] leading-[20px] font-bold text-black">
-                          {data?.teams?.away?.name}
-                        </p>
+                      <div className="block rounded-full text-center mb-0 bg-gray-800 hover:bg-black">
+                        <Link
+                          className="w-full font-Syne font-bold flex items-center justify-center 
+                        px-4 py-0 text-[15px] leading-[44px] from-[#03faa1] via-[#06c5d5] to-[#08a5f5] 
+                        bg-gradient-to-r bg-clip-text text-transparent"
+                          onClick={() =>
+                            openViewDetailsModalHandle(
+                              `${data?.fixture?.id}:${data?.teams?.home?.id}:${data?.teams?.away?.id}:${data?.fixture?.timestamp}`
+                            )
+                          }
+                        >
+                          View Details
+                          <FiArrowRight className="text-[#08a5f5] ml-0.5" />
+                        </Link>
                       </div>
-                    </div>
-                    <div className="block rounded-full text-center mb-0 bg-gray-800 hover:bg-black">
-                      <Link
-                        className="w-full font-Syne font-bold flex items-center justify-center 
-                    px-4 py-0 text-[15px] leading-[44px] from-[#03faa1] via-[#06c5d5] to-[#08a5f5] 
-                    bg-gradient-to-r bg-clip-text text-transparent"
-                        onClick={() =>
-                          openViewDetailsModalHandle(
-                            `${data?.fixture?.id}:${data?.teams?.home?.id}:${data?.teams?.away?.id}:${data?.fixture?.timestamp}`
-                          )
-                        }
-                      >
-                        View Details
-                        <FiArrowRight className="text-[#08a5f5] ml-0.5" />
-                      </Link>
                     </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
           </div>
         ) : (
           <div className="text-center">
